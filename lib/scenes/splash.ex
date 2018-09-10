0f20bbc418d249e118a744c46b203d8f8ad71838
Jason Tu <jasontu4@gmail.com>
2018-09-10 07:33:06 -0700

    super hacky wow

--- lib/scenes/splash.ex
+++ lib/scenes/splash.ex
@@ -20,10 +20,6 @@ defmodule MyApp.Scene.Splash do
     Scenic.Cache.Hash.compute(data, :sha)
   end
 
-  # @parrot_hash  "UfHCVlANI2cFbwSpJey64FxjT-0"
-  # @parrot_hash  "o85utvWj30E_3qWopRed9oOlauc"
-  #@parrot_hash  "23ndsle1ASB5xHQVTHsiFcm3a3w"
-  #@parrot_hash  "ABDvsbh7k4z3OYi9CjWlgf4iRiE"
   @parrot_hash  "-5ZxQ0CtZmwu-2fX0N4na_lXLZQ"
   @parrot_path  :code.priv_dir(:my_app)
               |> Path.join( "/static/images/awesome_original2.png" )
@@ -41,37 +37,32 @@ defmodule MyApp.Scene.Splash do
     root_graph = Graph.build()
     |> rect(
       {@parrot_width, @parrot_height}, id: :parrot,
-      fill: {:image, {@parrot_hash, 1}}
-    )
-    |> rect(
-      {@parrot_width, @parrot_height}, id: :not_parrot,
-      fill: {:image, {@parrot_hash, 1}}
+      fill: {:image, {@parrot_hash, 255}}
     )
     |> rect({5, 100}, fill: :white, id: :left)
     |> rect({5, 100}, fill: :white, id: :right)
-
-
 
     # calculate the transform that centers the parrot in the viewport
     {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} = ViewPort.info(viewport)
     move = {
       (vp_width / 2) - (@parrot_width / 2),
-      (vp_height / 2) - (@parrot_height / 2)
+      (vp_height / 2) - (@parrot_height / 2)-100
     }
 
     # load the parrot texture into the cache
     Scenic.Cache.File.load( @parrot_path, :insecure)
 
     # move the parrot into the right location
-    graph = Graph.modify( root_graph, :parrot, &update_opts(&1, translate: move) )
-    |> push_graph()
-    graph = Graph.modify( root_graph, :not_parrot, &update_opts(&1, translate: move) )
+    # move paddles into right locations
+    graph = root_graph
+
+    graph = Graph.modify(graph, :parrot, &update_opts(&1, translate: move) )
+            |> push_graph()
+
+    graph = Graph.modify(graph, :left, &update_opts(&1, translate: {20, 10}) )
     |> push_graph()
 
-    # move paddles into right locations
-    graph = Graph.modify( root_graph, :left, &update_opts(&1, translate: {0, 10}) )
-    |> push_graph()
-    graph = Graph.modify( root_graph, :left, &update_opts(&1, translate: {400, 10}) )
+    graph = Graph.modify(graph, :right, &update_opts(&1, translate: {vp_width-20-10, 10}) )
     |> push_graph()
 
     # start a very simple animation timer
@@ -82,7 +73,8 @@ defmodule MyApp.Scene.Splash do
       timer: timer,
       graph: graph,
       first_scene: first_scene,
-      alpha: 255
+      alpha: 255,
+      vel: {2,2}
     }
 
     push_graph(graph)
@@ -94,61 +86,84 @@ defmodule MyApp.Scene.Splash do
   # A very simple animation. A timer runs, which increments a counter. The counter
   # Is applied as an alpha channel to the parrot png.
   # When it is fully saturated, transition to the first real scene
-#  def handle_info( :animate,
-#    %{timer: timer, alpha: a} = state
-#  ) when a >= 256 do
-#    :timer.cancel(timer)
-#    Process.send_after(self(), :finish, @finish_delay_ms)
-#    {:noreply, state}
-#  end
+  def handle_info( :animate, %{timer: timer, alpha: a} = state) do
+    {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} = ViewPort.info(state.viewport)
+#    move = {
+#      (vp_width / 2) - (@parrot_width / 2),
+#      (vp_height / 2) - (@parrot_height / 2)
+#    }
+
+#    if x+@parrot_width > vp_width do
+#      graph = Graph.modify(state.graph, :parrot, &update_opts(&1, translate: {x-2,y+2}) )
+#      |> push_graph()
+#    else
+
+    {x,y} = Graph.get!(state.graph, :parrot).transforms.translate
+    {vel_x, vel_y} = state.vel
+
+    {right_x,right_y} = Graph.get!(state.graph, :right).transforms.translate
+    {vel_x, vel_y} = state.vel
+
+    if x+@parrot_width > right_x do
+      if y+@parrot_height > vp_height do
+        graph = Graph.modify(state.graph, :parrot, &update_opts(&1, translate: {x-2,y}))
+        |> push_graph()
+        {:noreply, %{ state | vel: {-vel_x, -vel_y}, graph: graph }}
+      else
+        graph = Graph.modify(state.graph, :parrot, &update_opts(&1, translate: {x-2,y-2}))
+        |> push_graph()
+        {:noreply, %{ state | vel: {-vel_x, vel_y}, graph: graph }}
+      end
+    else
+      if y+@parrot_height > vp_height do
+        graph = Graph.modify(state.graph, :parrot, &update_opts(&1, translate: {x,y-2}))
+        |> push_graph()
+        {:noreply, %{ state | vel: {vel_x, -vel_y}, graph: graph }}
+      else
+        graph = Graph.modify(state.graph, :parrot, &update_opts(&1, translate: {x+vel_x,y+vel_y}))
+        |> push_graph()
+        {:noreply, %{ state | graph: graph }}
+      end
+    end
+
+
+    #:timer.cancel(timer)
+    #Process.send_after(self(), :finish, @finish_delay_ms)
+    #IO.puts("animate")
+  end
 
 #   def handle_info( :finish, state ) do
-#     go_to_first_scene( state )
 #     {:noreply, state }
 #   end
 
-  def handle_info( :animate, %{alpha: alpha, graph: graph} = state ) do
-    graph = graph
-    |> Graph.modify( :parrot, &update_opts(&1, fill: {:image, {@parrot_hash, alpha}}))
-    |> push_graph()
-    {:noreply, %{state | graph: graph, alpha: 255}}
-    #{:noreply, state}
-  end
-
-  # --------------------------------------------------------
-  # short cut to go right to the new scene on user input
-#   def handle_input( {:cursor_button, {_,:press,_,_}}, _context, state ) do
-#     IO.puts("in cursor handler")
-#     # go_to_first_scene( state )
-#     {:noreply, state}
-#   end
    def handle_input( {:key, {"W", _, _}}, _context, state ) do
      IO.puts("W")
-     # go_to_first_scene( state )
-     {:noreply, state}
+      {x,y} = Graph.get!(state.graph, :left).transforms.translate
+       graph = Graph.modify( state.graph, :left, &update_opts(&1, translate: {x,y-12}) )
+       |> push_graph()
+     {:noreply, %{ state | graph: graph }}
    end
    def handle_input( {:key, {"S", _, _}}, _context, state ) do
      IO.puts("S")
-     # go_to_first_scene( state )
-     {:noreply, state}
+      {x,y} = Graph.get!(state.graph, :left).transforms.translate
+       graph = Graph.modify( state.graph, :left, &update_opts(&1, translate: {x,y+12}) )
+       |> push_graph()
+     {:noreply, %{ state | graph: graph }}
    end
    def handle_input( {:key, {"I", _, _}}, _context, state ) do
      IO.puts("I")
-     # go_to_first_scene( state )
-     {:noreply, state}
+      {x,y} = Graph.get!(state.graph, :right).transforms.translate
+       graph = Graph.modify( state.graph, :right, &update_opts(&1, translate: {x,y-12}) )
+       |> push_graph()
+     {:noreply, %{ state | graph: graph }}
    end
    def handle_input( {:key, {"K", _, _}}, _context, state ) do
      IO.puts("K")
-     # go_to_first_scene( state )
-     {:noreply, state}
+      {x,y} = Graph.get!(state.graph, :right).transforms.translate
+       graph = Graph.modify( state.graph, :right, &update_opts(&1, translate: {x,y+12}) )
+       |> push_graph()
+     {:noreply, %{ state | graph: graph }}
    end
 
    def handle_input( _input, _context, state ), do: {:noreply, state}
-
-
-  # --------------------------------------------------------
-#   defp go_to_first_scene( %{viewport: vp, first_scene: first_scene} ) do
-#     ViewPort.set_root( vp, {first_scene, nil} )
-#   end
-
 end
